d79a98a4ef3765ca5fff66de9fc498e561c62fb3
Trevor Hedstrom <trevorhedstrom@gmail.com>
2026-04-02 01:32:38 -0700

    mouse drag emitter, vorticity confinement but it doesnt work

--- fluid-sim.py
+++ fluid-sim.py
@@ -10,7 +10,6 @@ INIT_LEAPFROG = 2
 
 ADVECT_GRID = 0
 ADVECT_PARTICLE = 1
-ADVECT_COFLIP_GRID = 2
 
 def get_asset_path(asset):
     return os.path.join(os.path.dirname(__file__), asset)
@@ -21,17 +20,14 @@ def is_down(input_state, key):
 class FluidSimulator:
     def __init__(self, device : spy.Device):
         self.device = device
-        self.enabled = False
         self.step_once = False
         self.initialized = False
         self.grid_vars = {}
         self.passes = {}
+        self.drag = None
 
     def setup_ui(self, window):
-        def toggle_callback():
-            self.enabled = not self.enabled
-            self.toggle_button.label = "Pause simulation" if self.enabled else "Resume simulation"
-        self.toggle_button = spy.ui.Button(window, "Resume simulation", callback=toggle_callback)
+        self.enabled = spy.ui.CheckBox(window, "Enabled", True)
 
         def step_callback():
             self.step_once = True
@@ -43,7 +39,7 @@ class FluidSimulator:
 
         self.init_mode  = spy.ui.ComboBox(window, "Init mode", 0, reset_callback, ["None", "Vortex", "Leapfrog"])
         self.emit_smoke = spy.ui.CheckBox(window, "Emit smoke")
-        self.advect_mode = spy.ui.ComboBox(window, "Advect mode", 0, items=["Grid", "Particle", "Grid (covector)"])
+        self.advect_mode = spy.ui.ComboBox(window, "Advect mode", 0, items=["Grid", "Particle"])
 
         self.resolution                  = spy.ui.DragInt2(window,  "Resolution", value=spy.int2(512,512), min=1, callback=reset_callback)
         self.advect_dt                   = spy.ui.DragFloat(window, "Advection dt", value=0.1, min=0)
@@ -51,7 +47,11 @@ class FluidSimulator:
         self.pressure_project_iterations = spy.ui.DragInt(window,   "Pressure projection iterations", value=50, min=1)
         self.avg_pressure = spy.ui.Text(window, "Avg pressure: 0")
         self.use_pressure_correction = spy.ui.CheckBox(window, "Pressure correction")
+        self.vorticity_confinement_amount = spy.ui.DragFloat(window, "Vorticity confinement")
     
+    def get_resolution(self):
+        return self.resolution.value
+
     def swap_grids(self):
         for name in ["velocity_x", "velocity_y", "pressure"]:
             name_out = f"{name}_rw"
@@ -63,42 +63,7 @@ class FluidSimulator:
             self.passes[id] = self.device.create_compute_kernel(self.device.load_program(shader, [entry]))
         self.passes[id].dispatch(dim, vars, command_encoder)
 
-    # must be called after updating velocities (advection), but before swapping the velocity grids
-    def pressure_project(self, command_encoder:spy.CommandEncoder):
-        if self.pressure_project_iterations.value == 0:
-            return
-        command_encoder.clear_texture_float(self.pressure_correction_0)
-        command_encoder.clear_texture_float(self.pressure_correction_1)
-        vars = {
-            "grid": self.grid_vars,
-            "dt":   self.advect_dt.value / self.advection_iterations.value,
-            "divergence":             self.divergence,
-            "pressure_correction":    self.pressure_correction_0,
-            "pressure_correction_rw": self.pressure_correction_1,
-        }
-
-        self.dispatch_pass(
-            "fluid-pressure-project.cs.slang",
-            "update_divergence",
-            self.grid_dispatch_dim,
-            vars,
-            command_encoder)
-        for iter in range(self.pressure_project_iterations.value):
-            self.dispatch_pass(
-                "fluid-pressure-project.cs.slang",
-                "step",
-                self.grid_dispatch_dim,
-                vars,
-                command_encoder)
-            vars["pressure_correction"], vars["pressure_correction_rw"] = vars["pressure_correction_rw"], vars["pressure_correction"]
-        self.dispatch_pass(
-            "fluid-pressure-project.cs.slang",
-            "apply",
-            self.grid_dispatch_dim,
-            vars,
-            command_encoder)
-
-    def initialize(self, command_encoder:spy.CommandEncoder):
+    def initialize_grids(self, command_encoder:spy.CommandEncoder):
         def create_grid(width, height, name, mip_count = 1):
             return self.device.create_texture(
                 format=spy.Format.r32_float,
@@ -108,7 +73,7 @@ class FluidSimulator:
                 usage=spy.TextureUsage.shader_resource | spy.TextureUsage.unordered_access,
                 label=name)
         
-        w,h = self.resolution.value
+        w,h = self.get_resolution()
 
         self.grid_vars = {
             "velocity_x":     create_grid(w+1, h, "velocity_x_0"),
@@ -118,11 +83,13 @@ class FluidSimulator:
             "pressure":       create_grid(w, h,   "pressure_0", spy.ALL_MIPS),
             "pressure_rw":    create_grid(w, h,   "pressure_1", spy.ALL_MIPS),
             "linear_sampler": self.device.create_sampler(),
-            "resolution":     self.resolution.value,
+            "resolution":     self.get_resolution(),
         }
         self.divergence            = create_grid(w, h, "divergence")
         self.pressure_correction_0 = create_grid(w, h, "pressure_correction_0")
         self.pressure_correction_1 = create_grid(w, h, "pressure_correction_1")
+
+        self.curl = create_grid(w, h, "curl")
 
         self.particle_map = ParticleMap(self.device, w*h, 4*w*h)
 
@@ -140,8 +107,8 @@ class FluidSimulator:
             usage=spy.BufferUsage.copy_destination
         )
 
-        self.grid_dispatch_dim     = [self.resolution.value.x,   self.resolution.value.y,   1]
-        self.mac_grid_dispatch_dim = [self.resolution.value.x+1, self.resolution.value.y+1, 1]
+        self.grid_dispatch_dim     = [w,   h,   1]
+        self.mac_grid_dispatch_dim = [w+1, h+1, 1]
 
         for name in ["velocity_x", "velocity_y", "pressure"]:
             command_encoder.clear_texture_float(self.grid_vars[name])
@@ -168,63 +135,128 @@ class FluidSimulator:
 
         self.initialized = True
 
+    def emit_drag(self, drag_start, drag_end):
+        self.drag = (drag_start, drag_end)
+
+    def advect(self, command_encoder:spy.CommandEncoder):
+        vars = {
+            "grid": self.grid_vars,
+            "particle_map": self.particle_map.vars,
+            "advect_dt": self.advect_dt.value / self.advection_iterations.value
+        }
+        if self.advect_mode.value == ADVECT_GRID:
+            self.dispatch_pass(
+                "fluid-advection.cs.slang",
+                "advect_grid",
+                self.mac_grid_dispatch_dim,
+                vars,
+                command_encoder)
+        elif self.advect_mode.value == ADVECT_PARTICLE:
+            self.particle_map.clear(command_encoder)
+            self.dispatch_pass(
+                "fluid-advection.cs.slang",
+                "advect_particle",
+                self.grid_dispatch_dim,
+                vars,
+                command_encoder)
+            self.particle_map.sort(command_encoder)
+            self.dispatch_pass(
+                "fluid-advection.cs.slang",
+                "particle_to_grid",
+                self.mac_grid_dispatch_dim,
+                vars,
+                command_encoder)
+
+    def add_vorticity_confinement(self, command_encoder:spy.CommandEncoder):
+        vars = {
+            "grid": self.grid_vars,
+            "curl": self.curl,
+            "confinement_scale": self.vorticity_confinement_amount.value
+        }
+
+        self.dispatch_pass(
+            "fluid-vorticity-confinement.cs.slang",
+            "compute_curl",
+            self.grid_dispatch_dim,
+            vars,
+            command_encoder)
+        
+        self.dispatch_pass(
+            "fluid-vorticity-confinement.cs.slang",
+            "add_confinement_force",
+            self.mac_grid_dispatch_dim,
+            vars,
+            command_encoder)
+
+    def pressure_project(self, command_encoder:spy.CommandEncoder):
+        if self.pressure_project_iterations.value <= 0:
+            return
+        command_encoder.clear_texture_float(self.pressure_correction_0)
+        command_encoder.clear_texture_float(self.pressure_correction_1)
+        vars = {
+            "grid": self.grid_vars,
+            "dt":   self.advect_dt.value / self.advection_iterations.value,
+            "divergence":             self.divergence,
+            "pressure_correction":    self.pressure_correction_0,
+            "pressure_correction_rw": self.pressure_correction_1,
+        }
+
+        self.dispatch_pass(
+            "fluid-pressure-project.cs.slang",
+            "update_divergence",
+            self.grid_dispatch_dim,
+            vars,
+            command_encoder)
+        for _ in range(self.pressure_project_iterations.value):
+            self.dispatch_pass(
+                "fluid-pressure-project.cs.slang",
+                "step",
+                self.grid_dispatch_dim,
+                vars,
+                command_encoder)
+            vars["pressure_correction"], vars["pressure_correction_rw"] = vars["pressure_correction_rw"], vars["pressure_correction"]
+        self.dispatch_pass(
+            "fluid-pressure-project.cs.slang",
+            "apply",
+            self.grid_dispatch_dim,
+            vars,
+            command_encoder)
+
     def step(self, command_encoder:spy.CommandEncoder, dt):
         if not self.initialized:
-            self.initialize(command_encoder)
+            self.initialize_grids(command_encoder)
 
-        if not self.enabled and not self.step_once:
+        if not self.enabled.value and not self.step_once:
             return
         
         self.step_once = False
 
-        if self.emit_smoke.value:
-            self.dispatch_pass(
-                "fluid-init.cs.slang",
-                "emit_smoke",
-                self.mac_grid_dispatch_dim,
-                { "grid": self.grid_vars },
-                command_encoder)
-            self.pressure_project(command_encoder)
-            self.swap_grids()
-
         for _ in range(self.advection_iterations.value):
-            vars = { "grid": self.grid_vars,
-                    "particle_map": self.particle_map.vars,
-                    "advect_dt": self.advect_dt.value / self.advection_iterations.value }
-            
             if self.use_pressure_correction.value:
                 command_encoder.generate_mips(self.grid_vars["pressure"])
+
+            self.advect(command_encoder)
             
-            if self.advect_mode.value == ADVECT_GRID:
+            if self.vorticity_confinement_amount.value > 0:
+                self.add_vorticity_confinement(command_encoder)
+
+            if self.emit_smoke.value:
                 self.dispatch_pass(
-                    "fluid-advection.cs.slang",
-                    "advect_grid",
+                    "fluid-init.cs.slang",
+                    "emit_smoke",
                     self.mac_grid_dispatch_dim,
-                    vars,
+                    { "grid": self.grid_vars },
                     command_encoder)
-            elif self.advect_mode.value == ADVECT_PARTICLE:
-                self.particle_map.clear(command_encoder)
+            if self.drag is not None:
+                drag_start, drag_end = self.drag
+                self.drag = None
                 self.dispatch_pass(
-                    "fluid-advection.cs.slang",
-                    "advect_particle",
-                    self.grid_dispatch_dim,
-                    vars,
-                    command_encoder)
-                self.particle_map.sort(command_encoder)
-                self.dispatch_pass(
-                    "fluid-advection.cs.slang",
-                    "particle_to_grid",
+                    "fluid-init.cs.slang",
+                    "emit_drag",
                     self.mac_grid_dispatch_dim,
-                    vars,
+                    { "grid": self.grid_vars, "drag_start": drag_start, "drag_end": drag_end },
                     command_encoder)
-            elif self.advect_mode.value == ADVECT_COFLIP_GRID:
-                self.dispatch_pass(
-                    "fluid-advection.cs.slang",
-                    "advect_grid_covector",
-                    self.mac_grid_dispatch_dim,
-                    vars,
-                    command_encoder)
-            
+                
             self.pressure_project(command_encoder)
             
             if self.use_pressure_correction.value:
@@ -289,7 +321,7 @@ class App:
         
         self.simulator.setup_ui(spy.ui.Group(window, label="Simulation"))
 
-        self.render_mode = spy.ui.ComboBox(window, "Render", 0, items=[ "Pressure", "Velocity", "Divergence" ])
+        self.render_mode = spy.ui.ComboBox(window, "Render", 0, items=[ "Pressure", "Velocity", "Divergence", "Curl" ])
 
     def on_resize(self, width: int, height: int):
         self.device.wait()
@@ -335,6 +367,45 @@ class App:
 
             self.ui.begin_frame(surface_texture.width, surface_texture.height)
 
+            screen_size = spy.uint2(surface_texture.width, surface_texture.height)
+            fluid_dim = self.simulator.get_resolution()
+            rect_pos = spy.int2(
+                int(screen_size.x/2 - self.render_scale*fluid_dim.x/2 - self.render_offset.x),
+                int(screen_size.y/2 - self.render_scale*fluid_dim.y/2 - self.render_offset.y)
+            )
+            rect_size = spy.int2(
+                int(fluid_dim.x * self.render_scale),
+                int(fluid_dim.y * self.render_scale),
+            )
+            
+            if is_down(self.input_state, spy.MouseButton.left) and "mouse" in self.input_state:
+                if self.drag_start is not None:
+                    if is_down(self.input_state, spy.KeyCode.left_shift):
+                        def screen_to_grid(p):
+                            uv = (p - rect_pos) / spy.float2(rect_size.x, rect_size.y)
+                            return uv * spy.float2(fluid_dim.x, fluid_dim.y)
+                        self.simulator.emit_drag(screen_to_grid(self.drag_start), screen_to_grid(self.input_state["mouse"]))
+                    else:
+                        self.render_offset -= self.input_state["mouse"] - self.drag_start
+                self.drag_start = self.input_state["mouse"]
+            else:
+                self.drag_start = None
+
+            scroll = self.input_state["scroll"]
+            if scroll != 0:
+                zoom_factor = 1 + .05 * scroll
+                if "mouse" in self.input_state:
+                    mouse = self.input_state["mouse"]
+                    cx, cy = screen_size.x / 2, screen_size.y / 2
+                    self.render_offset = spy.float2(
+                        self.render_offset.x + (zoom_factor - 1) * (mouse.x - cx + self.render_offset.x),
+                        self.render_offset.y + (zoom_factor - 1) * (mouse.y - cy + self.render_offset.y)
+                    )
+                self.render_scale *= zoom_factor
+
+            self.fps_text.text = f"FPS: {self.fps_avg:.2f}"
+
+
             command_encoder = self.device.create_command_encoder()
 
             self.simulator.step(command_encoder, dt)
@@ -347,17 +418,6 @@ class App:
                     usage=spy.TextureUsage.shader_resource | spy.TextureUsage.unordered_access | spy.TextureUsage.render_target,
                     label="render_texture",
                 )
-
-            screen_size = spy.uint2(surface_texture.width, surface_texture.height)
-            fluid_dim = self.simulator.grid_vars["resolution"]
-            rect_pos = spy.int2(
-                int(screen_size.x/2 - self.render_scale*fluid_dim.x/2 - self.render_offset.x),
-                int(screen_size.y/2 - self.render_scale*fluid_dim.y/2 - self.render_offset.y)
-            )
-            rect_size = spy.int2(
-                int(fluid_dim.x * self.render_scale),
-                int(fluid_dim.y * self.render_scale),
-            )
 
             self.render_shader.dispatch(
                 thread_count=[self.render_texture.width, self.render_texture.height, 1],
@@ -372,21 +432,6 @@ class App:
             )
             
             command_encoder.blit(surface_texture, self.render_texture)
-
-            self.fps_text.text = f"FPS: {self.fps_avg:.2f}"
-
-            if is_down(self.input_state, spy.MouseButton.left) and "mouse" in self.input_state:
-                if self.drag_start is not None:
-                    if is_down(self.input_state, spy.KeyCode.left_shift):
-                        uv0 = (self.drag_start - self.render_offset) / spy.float2(rect_size.x, rect_size.y)
-                        uv1 = (self.input_state["mouse"] - self.render_offset) / spy.float2(rect_size.x, rect_size.y)
-                        # self.simulator.emit_drag(command_encoder, uv0, uv1, 30/fluid_dim.x, 1/fluid_dim.x)
-                    else:
-                        self.render_offset -= self.input_state["mouse"] - self.drag_start
-                self.drag_start = self.input_state["mouse"]
-            else:
-                self.drag_start = None
-            self.render_scale *= 1 + .05*self.input_state["scroll"]
 
             self.ui.end_frame(surface_texture, command_encoder)
 
