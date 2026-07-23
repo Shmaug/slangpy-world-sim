22a2f5b2dc70e679eeab540577ad843db2495512
Trevor Hedstrom <trevorhedstrom@gmail.com>
2026-07-22 19:47:33 -0700

    multires seems to work (thx claude)

--- fluid_cubemap/CubeFluidSimulator.py
+++ fluid_cubemap/CubeFluidSimulator.py
@@ -3,7 +3,7 @@ import numpy as np
 import os
 
 class CubeFluidSimulator:
-    def __init__(self, device:spy.Device, widget:spy.ui.Widget|None = None):
+    def __init__(self, device:spy.Device, widget:spy.ui.Widget):
         self.device = device
 
         def create_kernel(shader_file, entry):
@@ -14,9 +14,11 @@ class CubeFluidSimulator:
         self.emit_kernel = create_kernel("fluid-init.cs.slang", "emit_plume")
         self.advect_kernel = create_kernel("fluid-advection.cs.slang", "advect")
         self.conserve_smoke_kernel = create_kernel("fluid-conservation.cs.slang", "conserve_smoke")
-        self.compute_divergence_kernel     = create_kernel("fluid-pressure-project.cs.slang", "compute_divergence")
-        self.pressure_project_kernel       = create_kernel("fluid-pressure-project.cs.slang", "step")
-        self.pressure_project_apply_kernel = create_kernel("fluid-pressure-project.cs.slang", "apply")
+        self.compute_divergence_kernel       = create_kernel("fluid-pressure-project.cs.slang", "compute_divergence")
+        self.pressure_project_step_kernel    = create_kernel("fluid-pressure-project.cs.slang", "step")
+        self.pressure_project_f2c_kernel = create_kernel("fluid-pressure-project.cs.slang", "fine_to_coarse")
+        self.pressure_project_c2f_kernel = create_kernel("fluid-pressure-project.cs.slang", "coarse_to_fine")
+        self.pressure_project_apply_kernel   = create_kernel("fluid-pressure-project.cs.slang", "apply")
 
         self.reset = True
         self.step_once = False
@@ -26,7 +28,7 @@ class CubeFluidSimulator:
         self.divergence_tex:spy.Texture = None # type:ignore
         self.pressure_correction_tex:list[spy.Texture] = []
 
-        self.resolution = 1<<10
+        self.resolution = 512
         def create_textures():
             self.smoke_tex = [self.device.create_texture(
                 type = spy.TextureType.texture_2d,
@@ -48,6 +50,7 @@ class CubeFluidSimulator:
                 format = spy.Format.rg32_float,
                 width = 6 * self.resolution,
                 height = self.resolution,
+                mip_count = int(spy.math.ceil(spy.math.log2(self.resolution))) + 1,
                 usage = spy.TextureUsage.shader_resource | spy.TextureUsage.unordered_access
             )
             self.pressure_correction_tex = [ self.device.create_texture(
@@ -55,6 +58,7 @@ class CubeFluidSimulator:
                 format = spy.Format.r32_float,
                 width = 6 * self.resolution,
                 height = self.resolution,
+                mip_count = int(spy.math.ceil(spy.math.log2(self.resolution))) + 1,
                 usage = spy.TextureUsage.shader_resource | spy.TextureUsage.unordered_access
             ) for _ in range(2) ]
 
@@ -62,21 +66,22 @@ class CubeFluidSimulator:
 
         create_textures()
 
-        if widget is not None:
-            def reset_cb():
-                self.reset = True
-            def step_cb():
-                self.step_once = True
-            def res_cb(value):
-                self.resolution = min(max(1,1<<self.resolution_ui.value), 1<<12)
-                create_textures()
-            self.paused = spy.ui.CheckBox(widget, "Pause")
-            spy.ui.Button(widget, "Step", callback=step_cb)
-            self.reset_button = spy.ui.Button(widget, "Reset", callback=reset_cb)
-            self.resolution_ui = spy.ui.ComboBox(widget, "Resolution", 10, items=[ str(1 << i) for i in range(13) ], callback=res_cb)
-            self.solver_iterations = spy.ui.DragInt(widget, "Solver iterations", value=10)
-            self.dt = spy.ui.DragFloat(widget, "Timestep", 0.01)
-            self.emit_plume = spy.ui.CheckBox(widget, "Emit plume")
+        def reset_cb():
+            self.reset = True
+        def step_cb():
+            self.step_once = True
+        def res_cb(value):
+            self.resolution = min(max(1,1<<self.resolution_ui.value), 1<<12)
+            create_textures()
+        self.paused = spy.ui.CheckBox(widget, "Pause")
+        spy.ui.Button(widget, "Step", callback=step_cb)
+        self.reset_button = spy.ui.Button(widget, "Reset", callback=reset_cb)
+        self.resolution_ui = spy.ui.ComboBox(widget, "Resolution", int(spy.math.ceil(spy.math.log2(self.resolution))), items=[ str(1 << i) for i in range(13) ], callback=res_cb)
+        self.solver_iterations = spy.ui.DragInt(widget, "Solver iterations", value=5)
+        self.solver_fine_iterations = spy.ui.DragInt(widget, "Solver fine iterations", value=4)
+        self.multires_solve = spy.ui.CheckBox(widget, "Multiresolution solver", value=True)
+        self.dt = spy.ui.DragFloat(widget, "Timestep", 0.01)
+        self.emit_plume = spy.ui.CheckBox(widget, "Emit plume")
 
     def shader_vars(self):
         return {
@@ -92,7 +97,7 @@ class CubeFluidSimulator:
             vars[0], vars[1] = vars[1], vars[0]
 
         if self.reset:
-            for t in self.smoke_tex + self.velocity_tex:
+            for t in self.smoke_tex + self.velocity_tex + self.pressure_correction_tex:
                 command_encoder.clear_texture_float(t)
             self.reset = False
 
@@ -100,8 +105,9 @@ class CubeFluidSimulator:
             return
         self.step_once = False
 
-        def dispatch(kernel, vars):
-            kernel.dispatch([self.resolution, self.resolution, 6], vars, command_encoder)
+        def dispatch(kernel, vars, mip = 0):
+            res = self.resolution >> mip
+            kernel.dispatch([res, res, 6], vars, command_encoder)
 
         # advect velocity and smoke
         dispatch(self.advect_kernel, {
@@ -121,17 +127,43 @@ class CubeFluidSimulator:
         })
 
         if self.solver_iterations.value > 0:
-            def pressure_project_vars():
+            def pressure_project_vars(mip = 0):
                 return {
                     "fluid": self.shader_vars(),
                     "divergence": self.divergence_tex,
+                    "divergence_rw": self.divergence_tex,
                     "pressure_correction": self.pressure_correction_tex[0],
-                    "pressure_correction_rw": self.pressure_correction_tex[1],
+                    "pressure_correction_rw": self.pressure_correction_tex[1].create_view(mip=mip, mip_count=1),
+                    "mip_level": mip,
                 }
+            
             dispatch(self.compute_divergence_kernel, pressure_project_vars())
-            for _ in range(self.solver_iterations.value):
-                dispatch(self.pressure_project_kernel, pressure_project_vars())
-                swap(self.pressure_correction_tex)
+
+            if self.multires_solve.value:
+                for mip in range(self.pressure_correction_tex[0].mip_count-1):
+                    dispatch(self.pressure_project_f2c_kernel, pressure_project_vars(mip+1))
+
+                for _ in range(self.solver_iterations.value):
+                    # pre-smooth on the fine level
+                    for _ in range(self.solver_fine_iterations.value):
+                        dispatch(self.pressure_project_step_kernel, pressure_project_vars())
+                        swap(self.pressure_correction_tex)
+
+                    # fine -> coarse via mipmapping
+                    command_encoder.generate_mips(self.pressure_correction_tex[0])
+                    for i in range(2, self.pressure_correction_tex[0].mip_count):
+                        mip = self.pressure_correction_tex[0].mip_count - i - 1
+                        # interpolate mip+1 -> mip
+                        dispatch(self.pressure_project_c2f_kernel, pressure_project_vars(mip), mip=mip)
+                        swap(self.pressure_correction_tex)
+                        # solve on mip
+                        dispatch(self.pressure_project_step_kernel, pressure_project_vars(mip), mip=mip)
+                        swap(self.pressure_correction_tex)
+            else:
+                for _ in range(self.solver_iterations.value):
+                    dispatch(self.pressure_project_step_kernel, pressure_project_vars())
+                    swap(self.pressure_correction_tex)
+
             dispatch(self.pressure_project_apply_kernel, pressure_project_vars())
 
         if self.emit_plume.value:
